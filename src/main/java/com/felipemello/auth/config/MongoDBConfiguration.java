916a2354da30bea769ffc5277a150aed72f44f91
unknown <felipe.silva.mello@gmail.com>
2020-01-11 20:21:09 +0000

    swagger-ui configured

--- src/main/java/com/felipemello/auth/config/MongoDBConfiguration.java
+++ src/main/java/com/felipemello/auth/config/MongoDBConfiguration.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
-import com.mongodb.ConnectionString;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 
@@ -19,7 +18,7 @@ public class MongoDBConfiguration {
     @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
     public MongoClient mongoClient(@Value("${spring.data.mongodb.uri}") String connectionString) {
 
-      ConnectionString connString = new ConnectionString(connectionString);
+//      ConnectionString connString = new ConnectionString(connectionString);
 
       //TODO> Ticket: Handling Timeouts - configure the expected
       // WriteConcern `wtimeout` and `connectTimeoutMS` values
